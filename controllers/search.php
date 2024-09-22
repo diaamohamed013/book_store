48a0c3b58fcf8030666b5ad305384fc3c59eb606
diaamohamed013 <diaamohamed013@gmail.com>
2024-09-22 19:23:59 +0300

    edit search

--- controllers/search.php
+++ controllers/search.php
@@ -15,14 +15,14 @@ if (isset($_POST['query'])) {
 
     // Search query
 
-    $sql = "SELECT books.id , authors.author_name FROM books INNER JOIN authors ON books.auth_id = authors.id WHERE author_name LIKE '%$searchQuery%'";
+    $sql = "SELECT *  FROM books WHERE `title` LIKE '%$searchQuery%'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             echo "<li class='list-group-item' style='padding: 7px; border-bottom: 1px solid #ddd;'>
         <a href='" . url("single_product") . "&id=" . $row['id'] . "' style='text-decoration: none; color: #333; display: block;'>"
-                . $row['author_name'] .
+                . $row['title'] .
                 "</a>
       </li>";
         }
